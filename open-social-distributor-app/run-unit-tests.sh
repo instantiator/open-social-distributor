549c703afecb54aa1e503964b54caf491b13f4b2
Lewis Westbury <lewis@cantab.net>
2023-07-30 21:10:54 +0100

    Update unit tests script to fail on failure

--- open-social-distributor-app/run-unit-tests.sh
+++ open-social-distributor-app/run-unit-tests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 echo "Running unit tests..."
 dotnet test test/DistributorLib.Tests/DistributorLib.Tests.csproj
 dotnet test test/DistributionFunction.Tests/DistributionFunction.Tests.csproj
