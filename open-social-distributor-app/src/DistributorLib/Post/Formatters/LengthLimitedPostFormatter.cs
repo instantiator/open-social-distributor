e13b2510dfcb03b5326dbe8ff75d9dee43dd06f4
Lewis Westbury <lewis@cantab.net>
2023-08-29 21:23:34 +0100

    Fix word wrap formatting

--- open-social-distributor-app/src/DistributorLib/Post/Formatters/LengthLimitedPostFormatter.cs
+++ open-social-distributor-app/src/DistributorLib/Post/Formatters/LengthLimitedPostFormatter.cs
@@ -16,50 +16,117 @@ public class LengthLimitedPostFormatter : AbstractPostFormatter
 
     public override IEnumerable<string> FormatText(ISocialMessage message)
     {
-        var included = message.Parts.Where(part => linkInText || part.Part != SocialMessagePart.Link);
-        var strings = included.Select(part => part.ToStringFor(Network)).Where(x => x != null).ToList();
-        var words = string.Join(' ', strings).Split(' ');
+        return new WordWrapFormatter()
+            .WithNetwork(Network)
+            .WithLimit(messageLengthLimit)
+            .WithMessage(message)
+            .WithLinkInText(linkInText)
+            .Build();
+    }
 
-        var messages = new List<string>();
-        var currentMessageIndex = 1;
-        var currentMessage = new StringBuilder();
-        var firstInMessage = true;
+    public class WordWrapFormatter
+    {
+        public List<string>? Messages { get; private set; }
 
-        for (int w = 0; w < words.Count(); w++)
+        private int currentMessageIndex;
+        private StringBuilder? currentMessage;
+        private ISocialMessage? message;
+        private NetworkType network;
+        private int limit;
+        private bool link;
+
+        public WordWrapFormatter WithNetwork(NetworkType network) { this.network = network; return this; }
+        public WordWrapFormatter WithLimit(int limit) { this.limit = limit; return this; }
+        public WordWrapFormatter WithMessage(ISocialMessage message) { this.message = message; return this; }
+        public WordWrapFormatter WithLinkInText(bool link) { this.link = link; return this; }
+
+        public IEnumerable<string> Build()
         {
-            var currentWord = words[w];
-            var currentWordLen = currentWord.Length;
+            var included = message!.Parts.Where(part => link || part.Part != SocialMessagePart.Link);
+            var strings = included.Select(part => part.ToStringFor(network)).Where(x => x != null).ToList();
+            var words = string.Join(' ', strings).Split(' ');
 
-            var nextWord = w + 1 < words.Count() ? words[w+1] : null;
-            var indexWord = $"/{currentMessageIndex}";
-            var followingWordLen = Math.Min(nextWord?.Length ?? 0, indexWord.Length);
+            Messages = new List<string>();
+            currentMessageIndex = 1;
+            currentMessage = new StringBuilder();
 
-            // if we cannot fit the word, append the index word, and start a new message
-            if (currentMessage.Length + currentWordLen + 1 >= messageLengthLimit)
+            for (int i = 0; i < words.Length; i++)
             {
-                if (!firstInMessage) { currentMessage.Append(" "); }
-                currentMessage.Append(indexWord);
-                messages.Add(currentMessage.ToString());
-                currentMessage = new StringBuilder();
-                currentMessageIndex++;
-                firstInMessage = true;
+                PerformAction(words, i);
             }
+            CompleteMessage();
+            return Messages;
+        }
 
-            // if the current word is longer than the message length limit, split it up
-            if (currentWordLen > messageLengthLimit)
-            {
-                messages.AddRange(currentWord.Chunk(messageLengthLimit).Select(s => new string(s)).ToList());
-            }
+        private void PerformAction(IEnumerable<string> words, int index)
+        {
+            var word = words.ElementAt(index);
+            var nextWord = index + 1 < words.Count() ? words.ElementAt(index+1) : null;
 
-            // if we can fit the word, and the next word OR the index word, append the word
-            if (currentMessage.Length + currentWordLen + followingWordLen + 2 <= messageLengthLimit)
+            var decision = DetermineAction(currentMessage!, word, nextWord, IndexWord(currentMessageIndex));
+            Console.WriteLine($"Decision: {decision} for: {word}");
+            switch (decision)
             {
-                if (!firstInMessage) { currentMessage.Append(" "); }
-                currentMessage.Append(currentWord);
-                firstInMessage = false;
+                case Decision.AddWord:
+                    currentMessage!.Append($"{(currentMessage!.Length == 0 ? "" : " ")}{word}");
+                    break;
+                case Decision.AddIndex:
+                    currentMessage!.Append($"{(currentMessage!.Length == 0 ? "" : " ")}{IndexWord(currentMessageIndex)}");
+                    CompleteMessage();
+                    PerformAction(words, index);
+                    break;
+                case Decision.AddNothing:
+                    CompleteMessage();
+                    PerformAction(words, index);
+                    break;
+                case Decision.VeryLongWord:
+                    CompleteMessage();
+                    var chunks = word.Chunk(limit).Select(s => new string(s)).ToList();
+                    foreach (var chunk in chunks)
+                    {
+                        currentMessage!.Append(chunk);
+                        CompleteMessage();
+                    }
+                    break;
+                case Decision.Finish:
+                    CompleteMessage();
+                    break;
             }
         }
 
-        return messages;
+        private string IndexWord(int index) => $"/{index}";
+
+        private void CompleteMessage()
+        {
+            if (currentMessage!.Length > 0) 
+            {
+                Messages!.Add(currentMessage!.ToString());
+                currentMessageIndex++;
+            }
+            currentMessage = new StringBuilder();
+        }
+
+        private enum Decision { AddWord, AddIndex, VeryLongWord, AddNothing, Finish }
+
+        private Decision DetermineAction(StringBuilder message, string? thisWord, string? nextWord, string indexWord)
+        {
+            if (thisWord == null) return Decision.Finish;
+            bool firstWordInMessage = message.Length == 0;
+            int prefixChars = firstWordInMessage ? 0 : 1;
+            bool canFitThisWord = message.Length + thisWord!.Length + prefixChars <= limit;
+            bool canFitIndexWord = message.Length + indexWord.Length + prefixChars <= limit;
+            bool canFitThisAndNextWord = nextWord == null ? false : message.Length + thisWord!.Length + nextWord!.Length + prefixChars + 1 <= limit;
+            bool canFitThisAndIndexWord = message.Length + thisWord!.Length + indexWord.Length + prefixChars + 1 <= limit;
+            bool isVeryLongWord = thisWord.Length > limit;
+
+            if (canFitThisAndNextWord || canFitThisAndIndexWord) return Decision.AddWord;
+            if (message.Length == 0 && canFitThisWord) return Decision.AddWord;
+            if (message.Length == 0 && isVeryLongWord) return Decision.VeryLongWord;
+
+            return canFitIndexWord
+                ? Decision.AddIndex
+                : Decision.AddNothing; // shouldn't happen, might happen I guess
+        }
+
     }
 }
--- open-social-distributor-app/test/DistributorLib.Tests/LengthLimitedPostFormatterTests.cs
+++ open-social-distributor-app/test/DistributorLib.Tests/LengthLimitedPostFormatterTests.cs
@@ -10,12 +10,62 @@ public class LengthLimitedPostFormatterTests
     public void LengthLimitedPostFormatter_FormatText_SplitsOnWords()
     {
         var formatter = new LengthLimitedPostFormatter(NetworkType.Any, 10, false);
-        var message = new SimpleSocialMessage("This is a test message");
+        var message = new SimpleSocialMessage("This is a test message indeed so");
         var result = formatter.FormatText(message);
-        Console.WriteLine(string.Join('\n', result));
-        Assert.Equal(3, result.Count());
+        Assert.Equal(5, result.Count());
         Assert.Equal("This is /1", result.ElementAt(0));
         Assert.Equal("a test /2", result.ElementAt(1));
-        Assert.Equal("message", result.ElementAt(2));
+        Assert.Equal("message /3", result.ElementAt(2));
+        Assert.Equal("indeed /4", result.ElementAt(3));
+        Assert.Equal("so", result.ElementAt(4));
     }
+
+    [Fact]
+    public void LengthLimitedPostFormatter_FormatText_HandlesAwkwardEdgeCase()
+    {
+        var formatter = new LengthLimitedPostFormatter(NetworkType.Any, 10, false);
+        var message = new SimpleSocialMessage("This is a test messageX indeed");
+        var result = formatter.FormatText(message);
+        Assert.Equal(4, result.Count());
+        Assert.Equal("This is /1", result.ElementAt(0));
+        Assert.Equal("a test /2", result.ElementAt(1));
+        Assert.Equal("messageX", result.ElementAt(2)); // couldn't quite squeeze in the index without breaking the word
+        Assert.Equal("indeed", result.ElementAt(3));
+    }
+
+    [Fact]
+    public void LengthLimitedPostFormatter_FormatText_HandlesVeryLongWords()
+    {
+        var formatter = new LengthLimitedPostFormatter(NetworkType.Any, 10, false);
+        var message = new SimpleSocialMessage("This is a test message WithAVeryLongWordThatRunsOn");
+        var result = formatter.FormatText(message);
+        Assert.Equal(6, result.Count());
+        Assert.Equal("This is /1", result.ElementAt(0));
+        Assert.Equal("a test /2", result.ElementAt(1));
+        Assert.Equal("message /3", result.ElementAt(2));
+        Assert.Equal("WithAVeryL", result.ElementAt(3));
+        Assert.Equal("ongWordTha", result.ElementAt(4));
+        Assert.Equal("tRunsOn", result.ElementAt(5));
+    }
+
+    [Fact]
+    public void LengthLimitedPostFormatter_FormatText_HandlesVeryLongWordsAndSubsequentWords()
+    {
+        var formatter = new LengthLimitedPostFormatter(NetworkType.Any, 10, false);
+        var message = new SimpleSocialMessage("This is a test message WithAVeryLongWordThatRuns and some words after it");
+        var result = formatter.FormatText(message);
+        Assert.Equal(11, result.Count());
+        Assert.Equal("This is /1", result.ElementAt(0));
+        Assert.Equal("a test /2", result.ElementAt(1));
+        Assert.Equal("message /3", result.ElementAt(2));
+        Assert.Equal("WithAVeryL", result.ElementAt(3));
+        Assert.Equal("ongWordTha", result.ElementAt(4));
+        Assert.Equal("tRuns", result.ElementAt(5));
+        Assert.Equal("and /7", result.ElementAt(6));
+        Assert.Equal("some /8", result.ElementAt(7));
+        Assert.Equal("words /9", result.ElementAt(8));
+        Assert.Equal("after /10", result.ElementAt(9));
+        Assert.Equal("it", result.ElementAt(10));
+    }
+
 }
